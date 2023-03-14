23a33aab30ce049c51d36f0e2f5da572d25dace2
Shubhamkz <Shubhampandeykz@gmail.com>
2023-03-14 11:59:23 +0530

    check authentication on creating the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,27 @@
-module.exports.home = function (req, res) {
-  console.log(req.cookies);
-  res.cookie("user_id", 25);
-  return res.render("home", {
-    title: "Home",
-  });
-};
+const Post = require("../models/post");
 
+module.exports.home = function (req, res) {
+  // console.log(req.cookies);
+  // res.cookie("user_id", 25);
+  // return res.render("home", {
+  //   title: "Home",
+  // });
+
+  //   Post.find({}, function (err, posts) {
+  //     return res.render("home", {
+  //       title: "Codiel | Home",
+  //       posts: posts,
+  //     });
+  //   });
+
+  //populate the user of each post
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "Codiel | Home",
+        posts: posts,
+      });
+    });
+};
 // module.exports.actionName = function(req, res){}
